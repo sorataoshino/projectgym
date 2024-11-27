d6d5eaf2cc67da9764968e5a88b47fac331e2dc2
sorataoshino <sorataoshino@gmail.com>
2024-11-27 20:14:36 +0100

    First Draft: Layout
    REEEEEE

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+//ROOT LAYOUT
+
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="about" options={{ title: 'About' }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
+
+//https://reactnavigation.org/docs/header-buttons/?config=dynamic
